f6aebbb080b794a07d268c97d69ddda6ca4dd9e1
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2015-03-29 17:30:38 -0400

    carousel loops continuously.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -20,17 +20,17 @@ function toggleButtonDropdown() {
 
 function scrollALittle() {
 	// Scroll the carousel a little
-	$('#memory-carousel')
+	$('#memory-carousel').css("right", "-=1");
 	var images = $('#memory-carousel .memory').get();
 	for (var index = 0; index < images.length; index++) {
 		// 1. Check to see if the image is out of the page's bounds
-		if (images[index].getBoundingClientRect.left+images[index].getBoundingClientRect.width < 0) {
+		if (images[index].getBoundingClientRect().left == $('body').width()) {
 			// 2. If it is, put it at the end of the queue
-			$('#memory-carousel').append(images[index].clone());
+			$('#memory-carousel').append($(images[index]).clone());
 		}
 	};
 	// 2. Set a timer to repeat the event
-	setTimeout(scrollALittle, 1000);
+	setTimeout(scrollALittle, 50);
 }
 
 // Attaches listeners to the DOM once it has loaded
