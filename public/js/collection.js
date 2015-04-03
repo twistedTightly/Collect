2e25d175fd1cfd8a52df81abf93dbb83ba387794
twistedTightly <mrauh1@nd.edu>
2015-04-03 15:18:36 -0400

    Spacing and animation refinements

--- public/js/collection.js
+++ public/js/collection.js
@@ -3,27 +3,28 @@ function showComments(event) {
 
 		var commentSection = $( event.target ).closest( ".comment-section" );
 
-		var bottom, memoryShift = 40; // Standard spacing between memories
+		var bottom, memoryShift; // Standard spacing between memories
 		var commentsHeight = commentSection.outerHeight();
 		var lowerSectionHeight = commentSection.closest( ".upper-section" ).next().outerHeight(true);
 		var memoryMediaHeight = commentSection.closest( ".memory-media" ).outerHeight(true);
 
 		if (commentSection.hasClass( "expanded" )) {
+			memoryShift = 100;
 			bottom = memoryMediaHeight
-						- commentsHeight
+						- commentsHeight - 8 // Bottom margin that is added to the hr element
 						// The height of the media icons for this memory
 						- (commentSection.prev().outerHeight(true) 
 							* (commentSection.closest( ".memory-media" ).children().length - 1));
 		} else {
 			bottom = -1 * (lowerSectionHeight + commentsHeight);
-			memoryShift += commentsHeight; // Shift following memories down the same height as the comments that will appear
+			memoryShift = 60 + commentsHeight; // Shift following memories down the same height as the comments that will appear
 		}
 		// Reposition the comments section, from the side bar to below the relevant memory
 		commentSection.css( "bottom", bottom );
 		commentSection.toggleClass( "expanded" );
 
 		// Shift the following memories down
-		commentSection.closest( ".memory" ).next( ".memory" ).children(0).css( "margin-top", memoryShift );
+		commentSection.closest( ".memory" ).next( ".memory" ).children( ":first-child" ).css( "margin-top", memoryShift );
 }
 
 // Attaches listeners to the DOM once it has loaded
