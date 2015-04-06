df182302cc65ab13110604ad48f95dfbb00d0ba5
twistedTightly <mrauh1@nd.edu>
2015-04-06 14:28:01 -0400

    Animation for additional inputs changes + to -

--- public/js/addModal.js
+++ public/js/addModal.js
@@ -1,7 +1,11 @@
 // Move comments HTML to sit just below the lower section of the memory
 function showAdditionalInput(event) {
 	$( event.target ).toggleClass( "show" );
-	$( event.target ).children( "span" ).text("-");
+	if ($( event.target ).hasClass( "show" )) {
+		$( event.target ).children( "span" ).text("-");
+	} else {
+		$( event.target ).children( "span" ).text("+");
+	}
 }
 
 // Attaches listeners to the DOM once it has loaded
