a08ad18fe47632270d73c90bf5c854a2c59e7c56
twistedTightly <mrauh1@nd.edu>
2015-04-06 23:58:34 -0400

    Titled icon sizing fixed, colored section icons added

--- public/js/addModal.js
+++ public/js/addModal.js
@@ -1,10 +1,23 @@
-var path = "../images/titled-icons/";
+var titledPath = "../images/titled-icons/";
 var titledIcons = {
-	"write": 	path + "1_write_icon_blue.svg",
-	"location": path + "2_visit_icon_blue.svg",
-	"picture": 	path + "3_picture_icon_blue.svg",
-	"listen": 	path + "4_listen_icon_blue.svg",
-	"connect": 	path + "5_connect_icon_blue.svg"
+	"write": 	{	"black": titledPath + "1_write_icon_black.svg",
+					"color": titledPath + "1_write_icon_blue.svg" },
+	"location": {	"black": titledPath + "2_visit_icon_black.svg",
+					"color": titledPath + "2_visit_icon_blue.svg" },
+	"picture": 	{	"black": titledPath + "3_picture_icon_black.svg",
+					"color": titledPath + "3_picture_icon_blue.svg" },
+	"listen": 	{	"black": titledPath + "4_listen_icon_black.svg",
+					"color": titledPath + "4_listen_icon_blue.svg" },
+	"connect": 	{	"black": titledPath + "5_connect_icon_black.svg",
+					"color": titledPath + "5_connect_icon_blue.svg" }
+};
+var untitledPath = "../images/icons/";
+var regularIcons = {
+	"write": 	untitledPath + "narrative_icon_black.svg",
+	"location": untitledPath + "location_icon_black.svg",
+	"picture": 	untitledPath + "picture_icon_black.svg",
+	"listen": 	untitledPath + "sound_icon_black.svg",
+	"connect": 	untitledPath + "connections_icon_black.svg"
 };
 
 // Replace the header with the "write" section
@@ -14,16 +27,16 @@ function showWriteInput(event) {
 
 	// Change media icons to have titles and animate
 	if ($( "#write.row" ).hasClass( "show" )) {
-		$( "li#write" ).children( "img" ).attr( "src", titledIcons["write"] );
-		$( "li#location" ).children( "img" ).attr( "src", titledIcons["location"] );
-		$( "li#picture" ).children( "img" ).attr( "src", titledIcons["picture"] );
-		$( "li#listen" ).children( "img" ).attr( "src", titledIcons["listen"] );
-		$( "li#connect" ).children( "img" ).attr( "src", titledIcons["connect"] );
-		$( "#add-memory-modal > ul li" ).css({
-				"height": "39px",
-				"width": "47px" 
-		});
+		$( "li#write" ).children( "img" ).attr( "src", titledIcons["write"]["color"] );
+		$( "li#location" ).children( "img" ).attr( "src", titledIcons["location"]["color"] );
+		$( "li#picture" ).children( "img" ).attr( "src", titledIcons["picture"]["color"] );
+		$( "li#listen" ).children( "img" ).attr( "src", titledIcons["listen"]["color"] );
+		$( "li#connect" ).children( "img" ).attr( "src", titledIcons["connect"]["color"] );
 
+		$( "#add-memory-modal > ul li" ).css({
+				"height": "42px",
+				"width": "71px" 
+		});
 		$( "#add-memory-modal li:not(:last-child)" ).css( "margin-right", "154px" );
 	} else {
 		$( "#add-memory-modal li:not(:last-child)" ).css( "margin-right", "100px" );
@@ -46,7 +59,7 @@ function showAdditionalInput(event) {
 function showOptionalInput(event) {
 	var optionalInputId = ".row#" + event.target.id;
 	$( optionalInputId ).toggleClass( "show" );
-	if (event.target.id != "write") {
+	if (event.target.id !== "write") {
 		$( event.target ).toggleClass( "bw" );
 	}
 
