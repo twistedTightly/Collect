6617e212c1846fc24e5153b5596c6bd3e9a8d45c
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2015-04-06 23:35:44 -0400

    JS timer implemented

--- public/js/template.js
+++ public/js/template.js
@@ -1,13 +1,36 @@
+var IDLE_TIMEOUT = 10; //seconds
+var idleSecondsCounter = 0;
+
+// Attaches listeners to the DOM once it has loaded
+$( document ).ready(function() {
+	// Toggles the pullout menu
+	showPulloutMenu();
+	// build the redirect listener that will reset the page after a certain amount of time
+	document.onclick = function() {
+	    idleSecondsCounter = 0;
+	};
+	document.onmousemove = function() {
+	    idleSecondsCounter = 0;
+	};
+	document.onkeypress = function() {
+	    idleSecondsCounter = 0;
+	};
+	window.setInterval(checkIdleTime, 1000);
+});
+
 function showPulloutMenu() {
 	$( "#pullout-menu-tab" ).click(function() {
 		$( '#pullout-menu' ).toggleClass( "show" );
 		$( '#pullout-menu-tab' ).toggleClass( "show" );
 		$( 'header' ).toggleClass( "shift" );
 	});
+
 }
 
-// Attaches listeners to the DOM once it has loaded
-$( document ).ready(function() {
-	// Toggles the pullout menu
-	showPulloutMenu();
-});
+function checkIdleTime() {
+    idleSecondsCounter++;
+    if (idleSecondsCounter >= IDLE_TIMEOUT) {
+        alert("Time expired!");
+        document.location.href = "http://collect.parseapp.com";
+    }
+}
