e500c30db5e548c6a74f8f856275308fc4fed67a
twistedTightly <mrauh1@nd.edu>
2015-04-06 20:16:55 -0400

    Titled icons added, animated, colored

--- public/js/addModal.js
+++ public/js/addModal.js
@@ -1,11 +1,30 @@
+var path = "../images/titled-icons/";
+var titledIcons = {
+	"write": 	path + "1_write_icon_blue.svg",
+	"location": path + "2_visit_icon_blue.svg",
+	"picture": 	path + "3_picture_icon_blue.svg",
+	"listen": 	path + "4_listen_icon_blue.svg",
+	"connect": 	path + "5_connect_icon_blue.svg"
+};
+
 // Replace the header with the "write" section
 function showWriteInput(event) {
 	$( "#write.row" ).addClass( "show" );
 	$( "#add-memory-modal > p" ).removeClass( "show" );
 
-	// Animate media icons
+	// Change media icons to have titles and animate
 	if ($( "#write.row" ).hasClass( "show" )) {
-		$( "#add-memory-modal li:not(:last-child)" ).css( "margin-right", "188px" );
+		$( "li#write" ).children( "img" ).attr( "src", titledIcons["write"] );
+		$( "li#location" ).children( "img" ).attr( "src", titledIcons["location"] );
+		$( "li#picture" ).children( "img" ).attr( "src", titledIcons["picture"] );
+		$( "li#listen" ).children( "img" ).attr( "src", titledIcons["listen"] );
+		$( "li#connect" ).children( "img" ).attr( "src", titledIcons["connect"] );
+		$( "#add-memory-modal > ul li" ).css({
+				"height": "39px",
+				"width": "47px" 
+		});
+
+		$( "#add-memory-modal li:not(:last-child)" ).css( "margin-right", "154px" );
 	} else {
 		$( "#add-memory-modal li:not(:last-child)" ).css( "margin-right", "100px" );
 	}
@@ -27,7 +46,11 @@ function showAdditionalInput(event) {
 function showOptionalInput(event) {
 	var optionalInputId = ".row#" + event.target.id;
 	$( optionalInputId ).toggleClass( "show" );
-	if ($( optionalInputId ).hasClass( "show" )) {
+	if (event.target.id != "write") {
+		$( event.target ).toggleClass( "bw" );
+	}
+
+	if ($( ".optional-input" ).hasClass( "show" )) {
 		$( "#optional" ).css( "margin-bottom", "104px" );
 	} else {
 		$( "#optional" ).css( "margin-bottom", "0px" );
