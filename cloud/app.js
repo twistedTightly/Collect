c66df951ebc5c68af6fe7dcacbf25e9d2979d820
twistedTightly <mrauh1@nd.edu>
2015-04-07 15:14:11 -0400

    Cleaned up indentation

--- cloud/app.js
+++ cloud/app.js
@@ -17,66 +17,66 @@ app.get('/', function(req, res) {
 
 // Custom collections
 app.get('/collection', function(req, res) {
-	res.render('collection', { 	username: 		'Username',
-								collectionName: 'Name of Collection',
-								memoryCount: '0 memories',
-								contributorCount: '0 contributors'});
+	res.render('collection', { 	username: 			'Username',
+								collectionName: 	'Name of Collection',
+								memoryCount: 		'0 memories',
+								contributorCount: 	'0 contributors'});
 });
 
 app.get('/collection/summer-in-co', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Summer in Colorado',
-								memoryCount: '3 memories',
-								contributorCount: '2 contributors'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'Summer in Colorado',
+								memoryCount: 		'3 memories',
+								contributorCount: 	'2 contributors'});
 });
 
 app.get('/collection/emmies-first-year', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Emmie’s First Year',
-								memoryCount: '5 memories',
-								contributorCount: '2 contributors'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'Emmie’s First Year',
+								memoryCount: 		'5 memories',
+								contributorCount: 	'2 contributors'});
 });
 
 app.get('/collection/nick-and-elizabeth', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Nick &amp; Elizabeth',
-								memoryCount: '3 memories',
-								contributorCount: '2 contributors'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'Nick &amp; Elizabeth',
+								memoryCount: 		'3 memories',
+								contributorCount: 	'2 contributors'});
 });
 
 app.get('/collection/jack-and-diane', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Jack &amp; Diane',
-								memoryCount: '4 memories',
-								contributorCount: '1 contributor'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'Jack &amp; Diane',
+								memoryCount: 		'4 memories',
+								contributorCount: 	'1 contributor'});
 });
 
 app.get('/collection/growing-up-at-the-cape', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Growing Up at the Cape',
-								memoryCount: '3 memories',
-								contributorCount: '1 contributor'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'Growing Up at the Cape',
+								memoryCount: 		'3 memories',
+								contributorCount: 	'1 contributor'});
 });
 
 app.get('/collection/as-a-child', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'As a Child',
-								memoryCount: '2 memories',
-								contributorCount: '1 contributor'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'As a Child',
+								memoryCount: 		'2 memories',
+								contributorCount: 	'1 contributor'});
 });
 
 app.get('/collection/yale-university', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Yale University',
-								memoryCount: '2 memories',
-								contributorCount: '1 contributor'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'Yale University',
+								memoryCount: 		'2 memories',
+								contributorCount: 	'1 contributor'});
 });
 
 app.get('/collection/family-stories', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Family Stories',
-								memoryCount: '1 memory',
-								contributorCount: '2 contributors'});
+	res.render('collection', { 	username: 			'Elizabeth Pollard',
+								collectionName: 	'Family Stories',
+								memoryCount: 		'1 memory',
+								contributorCount: 	'2 contributors'});
 });
 
 // Unused
