f7ef31fd3bace8b008d80420857107efd5db1ca0
twistedTightly <mrauh1@nd.edu>
2015-04-07 14:57:38 -0400

    Elizabeth's gallery names added

--- cloud/app.js
+++ cloud/app.js
@@ -15,25 +15,76 @@ app.get('/', function(req, res) {
 	res.render('homepage');
 });
 
+// Custom collections
 app.get('/collection', function(req, res) {
-	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: ''});
+	res.render('collection', { 	username: 		'Username',
+								collectionName: 'Name of Collection',
+								memoryCount: '0 memories',
+								contributorCount: '0 contributors'});
 });
 
 app.get('/collection/summer-in-co', function(req, res) {
 	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Summer in Colorado'});
+								collectionName: 'Summer in Colorado',
+								memoryCount: '3 memories',
+								contributorCount: '2 contributors'});
 });
 
 app.get('/collection/emmies-first-year', function(req, res) {
 	res.render('collection', { 	username: 		'Elizabeth Pollard',
-								collectionName: 'Emmie’s First Year'});
+								collectionName: 'Emmie’s First Year',
+								memoryCount: '5 memories',
+								contributorCount: '2 contributors'});
 });
 
+app.get('/collection/nick-and-elizabeth', function(req, res) {
+	res.render('collection', { 	username: 		'Elizabeth Pollard',
+								collectionName: 'Nick &amp; Elizabeth',
+								memoryCount: '3 memories',
+								contributorCount: '2 contributors'});
+});
+
+app.get('/collection/jack-and-diane', function(req, res) {
+	res.render('collection', { 	username: 		'Elizabeth Pollard',
+								collectionName: 'Jack &amp; Diane',
+								memoryCount: '4 memories',
+								contributorCount: '1 contributor'});
+});
+
+app.get('/collection/growing-up-at-the-cape', function(req, res) {
+	res.render('collection', { 	username: 		'Elizabeth Pollard',
+								collectionName: 'Growing Up at the Cape',
+								memoryCount: '3 memories',
+								contributorCount: '1 contributor'});
+});
+
+app.get('/collection/as-a-child', function(req, res) {
+	res.render('collection', { 	username: 		'Elizabeth Pollard',
+								collectionName: 'As a Child',
+								memoryCount: '2 memories',
+								contributorCount: '1 contributor'});
+});
+
+app.get('/collection/yale-university', function(req, res) {
+	res.render('collection', { 	username: 		'Elizabeth Pollard',
+								collectionName: 'Yale University',
+								memoryCount: '2 memories',
+								contributorCount: '1 contributor'});
+});
+
+app.get('/collection/family-stories', function(req, res) {
+	res.render('collection', { 	username: 		'Elizabeth Pollard',
+								collectionName: 'Family Stories',
+								memoryCount: '1 memory',
+								contributorCount: '2 contributors'});
+});
+
+// Unused
 app.get('/connections', function(req, res) {
 	res.render('connections', { username: 'Elizabeth Pollard'});
 });
 
+// Profiles
 app.get('/profile', function(req, res) {
 	res.render('profile', { username: 'Elizabeth Pollard',
 							age: '' });
@@ -45,7 +96,7 @@ app.post('/profile', function(req, res) {
 
 	// Get the rest of the user's information
 	var query = new Parse.Query(Parse.User);
-	query.equalTo('username', 'mrauh1@nd.edu'); //req.body.email);
+	query.equalTo('username', 'elizabeth@gmail.com'); //req.body.email);
 	query.find({
 		// Query was successful
 		success: function(user) {
